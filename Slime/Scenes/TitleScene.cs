4d34e84c8057a85b7f0d91ce5aaddae9e7c4e47c
ArifDev-coder <achadzainul67@gmail.com>
2026-04-10 15:16:20 +0700

    Add GameScene and TitleScene classes to manage game flow and UI

---
+++ Slime/Scenes/TitleScene.cs
@@ -0,0 +1,79 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Slime;
+using Slime.Scenes;
+
+namespace Slime.Scenes;
+
+public class TitleScene : Scene
+{
+    private const string DUNGEON_TEXT = "Dungeon";
+    private const string SLIME_TEXT = "Slime";
+    private const string PRESS_ENTER_TEXT = "Press Enter To Start";
+    private const float titleMultiplyFontSize = 5f;
+
+    private SpriteFont _font;
+
+    private Vector2 _dungeonTextPos;
+    private Vector2 _dungeonTextOrigin;
+    private Vector2 _slimeTextPos;
+    private Vector2 _slimeTextOrigin;
+    private Vector2 _pressEnterPos;
+    private Vector2 _pressEnterOrigin;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        Core.ExitOnEscape = true;
+
+        Vector2 size = _font.MeasureString(DUNGEON_TEXT);
+        _dungeonTextPos = new(640, 100);
+        _dungeonTextOrigin = size * titleMultiplyFontSize * 0.5f;
+
+        size = _font.MeasureString(SLIME_TEXT);
+        _slimeTextPos = new(757, 207);
+        _slimeTextOrigin = size * titleMultiplyFontSize * 0.5f;
+
+        size = _font.MeasureString(PRESS_ENTER_TEXT);
+        _pressEnterPos = new(640, 620);
+        _pressEnterOrigin = size * 0.5f;
+    }
+
+    public override void LoadContent()
+    {
+        _font = Core.Content.Load<SpriteFont>("fonts/04B_30");
+
+        base.LoadContent();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        if (Core.Input.Keyboard.WasKeyJustPressed(Keys.Enter))
+        {
+            Core.ChangeScene(new GameScene());
+        }
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        Core.GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+
+        Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        Color dropShadowColor = Color.Black * 0.5f;
+
+        Core.SpriteBatch.DrawString(_font, DUNGEON_TEXT, _dungeonTextPos + new Vector2(10, 10), dropShadowColor, 0.0f, _dungeonTextOrigin, titleMultiplyFontSize, SpriteEffects.None, 1.0f);
+
+        Core.SpriteBatch.DrawString(_font, DUNGEON_TEXT, _dungeonTextOrigin, Color.White, 0.0f, _dungeonTextOrigin, titleMultiplyFontSize, SpriteEffects.None, 1.0f);
+
+        Core.SpriteBatch.DrawString(_font, SLIME_TEXT, _slimeTextPos + new Vector2(10, 10), dropShadowColor, 0.0f, _slimeTextOrigin, titleMultiplyFontSize, SpriteEffects.None, 1.0f);
+
+        Core.SpriteBatch.DrawString(_font, SLIME_TEXT, _slimeTextPos, Color.White, 0.0f, _slimeTextOrigin, titleMultiplyFontSize, SpriteEffects.None, 1.0f);
+
+        Core.SpriteBatch.DrawString(_font, PRESS_ENTER_TEXT, _pressEnterPos, Color.White, 0.0f, _pressEnterOrigin, 1.0f, SpriteEffects.None, 0.0f);
+
+        Core.SpriteBatch.End();
+    }
+}
