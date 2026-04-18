35c10266a30f0a93114285f5fb11aa913677e4dd
ArifDev-coder <achadzainul67@gmail.com>
2026-04-18 21:37:33 +0700

    Add GameController and Slime classes for player input handling and slime movement logic

---
+++ Slime/GameObjects/Slime.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Slime;
+using Slime.Graphics;
+
+namespace Slime.GameObjects;
+
+public class Slime
+{
+    private static readonly TimeSpan s_movementTime = TimeSpan.FromMilliseconds(200);
+
+    private TimeSpan _movementTimer;
+
+    private float _movementProgress;
+
+    private Vector2 _nextDirection;
+
+    private float _stride;
+
+    private List<SlimeSegment> _segments;
+
+    private AnimatedSprite _sprite;
+
+    public event EventHandler BodyCollision;
+
+    public Slime(AnimatedSprite sprite)
+    {
+        _sprite = sprite;
+    }
+
+    public void Initialize(Vector2 startingPosisition, float stride)
+    {
+        _segments = new List<SlimeSegment>();
+
+        _stride = stride;
+
+        SlimeSegment head = new SlimeSegment();
+        head.At = startingPosisition;
+        head.To = startingPosisition + new Vector2(_stride, 0);
+        head.Direction = Vector2.UnitX;
+
+        _segments.Add(head);
+
+        _nextDirection = head.Direction;
+
+        _movementTimer = TimeSpan.Zero;
+    }
+
+    private void HandleInput()
+    {
+        Vector2 potentialNextDirection = _nextDirection;
+
+        if (GameController.MoveUp())
+        {
+            potentialNextDirection = -Vector2.UnitY;
+        }
+        else if (GameController.MoveDown())
+        {
+            potentialNextDirection = Vector2.UnitY;
+        }
+        else if (GameController.MoveLeft())
+        {
+            potentialNextDirection = -Vector2.UnitX;
+        }
+        else if (GameController.MoveRigth())
+        {
+            potentialNextDirection = Vector2.UnitX;
+        }
+
+        float dot = Vector2.Dot(potentialNextDirection, _segments[0].Direction);
+        if (dot >= 0)
+        {
+            _nextDirection = potentialNextDirection;
+        }
+    }
+
+    private void Move()
+    {
+        SlimeSegment head = _segments[0];
+
+        head.Direction = _nextDirection;
+
+        head.At = head.To;
+
+        _segments.Insert(0, head);
+        _segments.RemoveAt(_segments.Count - 1);
+
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            SlimeSegment segment = _segments[i];
+
+            if (head.At == segment.At)
+            {
+                if (BodyCollision != null)
+                {
+                    BodyCollision.Invoke(this, EventArgs.Empty);
+                }
+            }
+
+            return;
+        }
+    }
+
+    public void Grow()
+    {
+        SlimeSegment tail = _segments[_segments.Count - 1];
+
+        SlimeSegment newTail = new SlimeSegment();
+        newTail.At = tail.To + tail.ReverseDirection * _stride;
+        newTail.To = tail.At;
+        newTail.Direction = Vector2.Normalize(tail.At - newTail.At);
+
+        _segments.Add(newTail);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        _sprite.Update(gameTime);
+
+        HandleInput();
+
+        _movementTimer += gameTime.ElapsedGameTime;
+
+        if (_movementTimer >= s_movementTime)
+        {
+            _movementTimer -= s_movementTime;
+            Move();
+        }
+
+        _movementProgress = (float)(_movementTimer.TotalSeconds / s_movementTime.TotalSeconds);
+    }
+}
